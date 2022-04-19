6a4d5517908b009853d6445b66a6744dbe8a5c5e
Snownee <i@snownee.com>
2022-04-19 18:17:13 +0800

    .

--- src/main/java/snownee/jade/addon/JadeAddons.java
+++ src/main/java/snownee/jade/addon/JadeAddons.java
@@ -3,11 +3,18 @@ package snownee.jade.addon;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import net.minecraftforge.fml.IExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.network.NetworkConstants;
 
 @Mod(JadeAddons.ID)
 public class JadeAddons {
 	public static final String ID = "jadeaddons";
 	public static final String NAME = "Jade Addons";
 	public static final Logger LOGGER = LogManager.getLogger(NAME);
+
+	public JadeAddons() {
+		ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+	}
 }
