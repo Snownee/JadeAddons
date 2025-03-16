465f8fe8cf4c1f70cf2d8592332c0de664308e80
SettingDust <settingdust@gmail.com>
2025-03-17 07:35:28 +0800

    chore: rename the plugin id

--- src/main/java/snownee/jade/addon/lootr/LootrPlugin.java
+++ src/main/java/snownee/jade/addon/lootr/LootrPlugin.java
@@ -13,7 +13,7 @@ import snownee.jade.api.IWailaCommonRegistration;
 import snownee.jade.api.IWailaPlugin;
 
 public class LootrPlugin implements IWailaPlugin {
-	public static final String ID = "lootr";
+	public static final String ID = "jadeaddons.lootr";
 	public static final ResourceLocation INFO = ResourceLocation.fromNamespaceAndPath(ID, "info");
 	public static final ResourceLocation INVENTORY = ResourceLocation.fromNamespaceAndPath(ID, "inventory");
 
